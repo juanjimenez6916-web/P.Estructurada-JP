62a1a27c55496d23e098f3c213550bd94731de64
juanjimenez6916-web <juan.jimenez6916@alumnos.udg.mx>
2026-02-13 08:37:32 -0600

    Create Numeroimpar.c

---
+++ Viernes 13 JP/Numeroimpar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int N;
+    printf("Ingrese un numero:",N);
+    scanf("%d",&N);
+    if(N%2==1){
+      printf("Es numero impar",N);
+    }
+    return 0;
+}
