c18cc979bf785b3840028a501a0fca8aa20ca2ee
juanjimenez6916-web <juan.jimenez6916@alumnos.udg.mx>
2026-02-13 08:31:19 -0600

    Create Numeropar.c

---
+++ Viernes 13 JP/Numeropar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int N;
+    printf("Ingrese un numero:",N);
+    scanf("%d",&N);
+    if(N%2==0){
+      printf("Es numero par",N);
+    }
+    return 0;
+}
