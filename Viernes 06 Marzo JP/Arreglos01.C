5d5cd47a62c89d1313a1714506106715e9cba97e
juanjimenez6916-web <juan.jimenez6916@alumnos.udg.mx>
2026-03-06 08:11:46 -0600

    Update Arreglos01.C

--- Viernes 06 Marzo JP/Arreglos01.C
+++ Viernes 06 Marzo JP/Arreglos01.C
@@ -5,10 +5,11 @@ int main()
 {
     float temp1, temp2;
     float temp [24];
+    int dummy;
     printf("Direccion de mem de 2 flotantes: &temp1:%i,&temp2 %i\n",&temp1,&temp2);
     printf("Direccion de temp[24]:%i\n",temp);
     printf("Direccion de &temp[24]:%i\n",&temp);
-
+    printf("Direccion de &dummy:%i\n",&dummy);
 
 
 
